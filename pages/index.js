373eff18cce9b58facd53b1b846207626cd7258a
Eric <Aerelioyanadel@gmail.com>
2020-01-02 13:15:52 -0500

    set render to backend instead of client side

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,23 @@
 import React from 'react'
 import axios from 'axios'
 
-function Home() {
-  React.useEffect(() => {
-    getProducts()
-  }, [])
+function Home({ products }) {
+  console.log(products)
+  // React.useEffect(() => {
+  //   getProducts()
+  // }, [])
 
-  async function getProducts(){
-    const url = "http://localhost:3000/api/products"
-    const response = await axios.get(url)
-    console.log(response)
-  }
+  // async function getProducts(){
+  
+  // }
 
   return <>home</>;
+}
+
+Home.getInitialProps = async() => {
+  const url = "http://localhost:3000/api/products"
+  const response = await axios.get(url)
+  return { products: response.data }
 }
 
 export default Home;
