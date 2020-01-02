4139b2456e2d0433267ac8c0fc40a58132f44217
Eric <Aerelioyanadel@gmail.com>
2020-01-02 12:42:18 -0500

    create get req from static image file

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,19 @@
+import React from 'react'
+import axios from 'axios'
+
 function Home() {
+  React.useEffect(() => {
+    getProducts()
+  }, [])
+
+  async function getProducts(){
+    const url = "http://localhost:3000/api/products"
+    const response = await axios.get(url)
+    console.log(response)
+  }
+
   return <>home</>;
 }
 
 export default Home;
+ 
