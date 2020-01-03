e225b1bc8c40de03d3b7b6fc859003f70863b0f6
Eric <Aerelioyanadel@gmail.com>
2020-01-02 19:06:45 -0500

    remove gitignore files

--- next.config.js
+++
@@ -1,9 +0,0 @@
-// must restart server whenever you make changes in next.config
-module.exports = {
-  env: {
-    MONGO_SRV: "mongodb+srv://Aereli:oyanadel@merncart-qtbu5.mongodb.net/test?retryWrites=true&w=majority ",
-    JWT_SECRET: "<insert-jwt-secret>",
-    CLOUDINARY_URL: "<insert-cloudinary-url>",
-    STRIPE_SECRET_KEY: "<insert-stripe-secret-key>"
-  }
-};
