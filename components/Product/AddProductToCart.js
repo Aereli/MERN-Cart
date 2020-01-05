597ca0aeef6a402aea0d9421e576c66d866cca43
Eric <Aerelioyanadel@gmail.com>
2020-01-05 13:11:32 -0500

    add unitque products

--- components/Product/AddProductToCart.js
+++ components/Product/AddProductToCart.js
@@ -1,5 +1,20 @@
+import { Input } from 'semantic-ui-react'
+
+
 function AddProductToCart() {
-  return <>AddProductToCart</>;
+  return <>
+    <Input 
+      type="number"
+      min="1"
+      placeholder="quantity"
+      value="1"
+      action={{
+        color: "teal",
+        content: "Add to Cart",
+        icon: "plus cart"
+      }}
+    />
+  </>
 }
 
 export default AddProductToCart;
--- components/Product/ProductAttributes.js
+++ components/Product/ProductAttributes.js
@@ -1,5 +1,16 @@
-function ProductAttributes() {
-  return <>ProductAttributes</>;
+import { Header, Button } from 'semantic-ui-react'
+
+function ProductAttributes({ description }) {
+  return <>
+      <Header as="h3">About this product</Header>
+      <p>{description}</p>
+      <Button 
+        icon="trash ternate outline"
+        color="red"
+        content="Delete Product"
+      />
+    </>
+  
 }
 
 export default ProductAttributes;
