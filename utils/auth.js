2cc0e0369240478d1dd3fe7e9573be5fb217d686
Eric <Aerelioyanadel@gmail.com>
2020-01-30 21:26:07 -0500

    add auth for redirect of urls

--- utils/auth.js
+++ utils/auth.js
@@ -1,7 +1,17 @@
 import cookie from "js-cookie"
 import Router from "next/router"
+import { CommentText } from "semantic-ui-react"
 
 export function handleLogin(token) {
   cookie.set("token", token)
   Router.push("/account")
+}
+
+export function redirectUser(ctx, location){
+  if(ctx.req){
+    ctx.res.writeHead(302, {Location: location})
+    ctx.res.end()
+  } else {
+    Router.push(location)
+  }
 }
