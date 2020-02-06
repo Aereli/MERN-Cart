5495e8e540ca384860106e6c4c081ff0604d573e
Eric <Aerelioyanadel@gmail.com>
2020-02-05 19:29:21 -0500

    first part in adding products to cart

--- components/Product/AddProductToCart.js
+++ components/Product/AddProductToCart.js
@@ -1,13 +1,16 @@
+import React from 'react'
 import { Input } from 'semantic-ui-react'
 
 
 function AddProductToCart() {
+  const [quantity, setQuantity] = React.useState(1)
   return <>
     <Input 
       type="number"
       min="1"
       placeholder="quantity"
-      value="1"
+      value={quantity}
+      onChange={event = setQuantity(event.target.value)}
       action={{
         color: "teal",
         content: "Add to Cart",
